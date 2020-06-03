f8e7339f4f959ca313cf1e9809d7dd5c30bb19e1
Ravindra Jain <avengerravi8055@gmail.com>
2020-06-03 12:16:40 +0530

    INitial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const connectDB = require("./config/db");
+
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+
+connectDB();
+// routes
+
+// Body parser
+app.use(express.json());
+// cors
+if (process.env.NODE_ENV == "development") {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
+
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
