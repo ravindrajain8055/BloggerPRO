9860ea7bd85ecba5868a9e5da3fc102443d31aa4
Ravindra Jain <avengerravi8055@gmail.com>
2020-06-12 16:45:04 +0530

    Google AUTH

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 // This will help us check if the token has expired or if it is valid
 const expressJwt = require('express-jwt');
 const { errorHandler } = require('../middleware/errorHandler');
+const { OAuth2Client } = require('google-auth-library');
 
 // access public
 // route POST /api/signup
@@ -127,4 +128,60 @@ exports.canUpdateDelete = (req, res, next) => {
     }
     next();
   });
+};
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+exports.googleLogin = (req, res) => {
+  const idToken = req.body.tokenId;
+  client.verifyIdToken(
+    { idToken, audience: process.env.GOOGLE_CLIENT_ID }.then(res => {
+      const { email_verified, name, email, jti } = res.payload;
+      if (email_verified) {
+        User.findOne({ email }).exec((err, user) => {
+          if (user) {
+            const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+              expiresIn: '60d',
+            });
+            res.cookie('token', token, { expiresIn: '60d' });
+            const { _id, email, name, role, username } = user;
+            return res.json({
+              token,
+              user: { _id, email, name, role, username },
+            });
+          } else {
+            let username = shortId.generate();
+            let profile = `${process.env.CLIENT_URL}/profile/${username}`;
+            let password = jti;
+            user = new User({ name, email, profile, username, profile });
+
+            user.save((err, data) => {
+              if (err) {
+                return res.status(400).json({
+                  error: errorHandler(err),
+                });
+              }
+
+              const token = jwt.sign(
+                { _id: data._id },
+                process.env.JWT_SECRET,
+                {
+                  expiresIn: '60d',
+                }
+              );
+              res.cookie('token', token, { expiresIn: '60d' });
+              const { _id, email, name, role, username } = data;
+              return res.json({
+                token,
+                user: { _id, email, name, role, username },
+              });
+            });
+          }
+        });
+      } else {
+        return res.status(400).json({
+          error: 'Google Login failed, Try aagain ',
+        });
+      }
+    })
+  );
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { signup, signin, signout } = require('../controllers/auth');
+const { signup, signin, signout, googleLogin } = require('../controllers/auth');
 const { userSignupValidator } = require('../validators/auth');
 const { userSigninValidator } = require('../validators/auth');
 const { runValidation } = require('../validators/index');
@@ -8,6 +8,7 @@ const { runValidation } = require('../validators/index');
 router.route('/signup').post(userSignupValidator, runValidation, signup);
 router.route('/signin').post(userSigninValidator, runValidation, signin);
 router.route('/signout').get(signout);
+router.post('/google-login', googleLogin);
 
 router.get('/login', (req, res) => {
   res.json({ success: true });
